b68a95f7af467d617d5ee8599845854b88ca8963
finnjohansen <finnjohansen4@gmail.com>
2014-03-11 12:14:22 +0100

    jaja

---
+++ src/ntnu/master/nofall/contentprovider/UserContentProvider.java
@@ -0,0 +1,187 @@
+package ntnu.master.nofall.contentprovider;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+import android.content.ContentProvider;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
+import android.net.Uri;
+import android.text.TextUtils;
+
+import ntnu.master.nofall.database.NoFallDBHelper;
+import ntnu.master.nofall.database.UserTable;
+
+
+public class UserContentProvider extends ContentProvider {
+	// database
+	private NoFallDBHelper database;
+
+	// used for the UriMacher
+	private static final int USER = 10;
+	private static final int USER_ID = 20;
+
+	private static final String AUTHORITY = "ntnu.master.nofall.contentprovider";
+
+	private static final String BASE_PATH = "user";
+	public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY
+			+ "/" + BASE_PATH);
+
+	public static final String CONTENT_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE
+			+ "/user";
+	public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE
+			+ "/user";
+
+	private static final UriMatcher sURIMatcher = new UriMatcher(
+			UriMatcher.NO_MATCH);
+	static {
+		sURIMatcher.addURI(AUTHORITY, BASE_PATH, USER);
+		sURIMatcher.addURI(AUTHORITY, BASE_PATH + "/#", USER_ID);
+	}
+
+	@Override
+	public boolean onCreate() {
+		database = new NoFallDBHelper(getContext());
+		return false;
+	}
+
+	@Override
+	public Cursor query(Uri uri, String[] projection, String selection,
+			String[] selectionArgs, String sortOrder) {
+
+		// Using SQLiteQueryBuilder instead of query() method
+		SQLiteQueryBuilder queryBuilder = new SQLiteQueryBuilder();
+
+		// check if the caller has requested a column which does not exists
+		checkColumns(projection);
+
+		// Set the table
+		queryBuilder.setTables(UserTable.TABLE_USER);
+
+		int uriType = sURIMatcher.match(uri);
+		switch (uriType) {
+		case USER:
+			break;
+		case USER_ID:
+			// adding the ID to the original query
+			queryBuilder.appendWhere(UserTable.COLUMN_ID + "="
+					+ uri.getLastPathSegment());
+			break;
+		default:
+			throw new IllegalArgumentException("Unknown URI: " + uri);
+		}
+
+		SQLiteDatabase db = database.getWritableDatabase();
+		Cursor cursor = queryBuilder.query(db, projection, selection,
+				selectionArgs, null, null, sortOrder);
+		
+		// make sure that potential listeners are getting notified
+		cursor.setNotificationUri(getContext().getContentResolver(), uri);
+
+		return cursor;
+	}
+
+	@Override
+	public String getType(Uri uri) {
+		return null;
+	}
+
+	@Override
+	public Uri insert(Uri uri, ContentValues values) {
+		int uriType = sURIMatcher.match(uri);
+		SQLiteDatabase sqlDB = database.getWritableDatabase();
+		int rowsDeleted = 0;		
+		long id = 0;
+
+		switch (uriType) {
+		case USER:
+			id = sqlDB.insert(UserTable.TABLE_USER, null, values);
+			break;
+		default:
+			throw new IllegalArgumentException("Unknown URI: " + uri);
+		}
+		getContext().getContentResolver().notifyChange(uri, null);
+		return Uri.parse(BASE_PATH + "/" + id);
+	}
+
+	@Override
+	public int delete(Uri uri, String selection, String[] selectionArgs) {
+		int uriType = sURIMatcher.match(uri);
+		SQLiteDatabase sqlDB = database.getWritableDatabase();
+		int rowsDeleted = 0;
+
+		switch (uriType) {
+		case USER:
+			rowsDeleted = sqlDB.delete(UserTable.TABLE_USER, selection,
+					selectionArgs);
+			break;
+		case USER_ID:
+			String id = uri.getLastPathSegment();
+			if (TextUtils.isEmpty(selection)) {
+				rowsDeleted = sqlDB.delete(UserTable.TABLE_USER,
+						UserTable.COLUMN_ID + "=" + id, null);
+			} else {
+				rowsDeleted = sqlDB.delete(UserTable.TABLE_USER,
+						UserTable.COLUMN_ID + "=" + id + " and " + selection,
+						selectionArgs);
+			}
+			break;
+		default:
+			throw new IllegalArgumentException("Unknown URI: " + uri);
+		}
+		getContext().getContentResolver().notifyChange(uri, null);
+		return rowsDeleted;
+	}
+
+	@Override
+	public int update(Uri uri, ContentValues values, String selection,
+			String[] selectionArgs) {
+
+		int uriType = sURIMatcher.match(uri);
+		SQLiteDatabase sqlDB = database.getWritableDatabase();
+		int rowsUpdated = 0;
+
+		switch (uriType) {
+		case USER:
+			rowsUpdated = sqlDB.update(UserTable.TABLE_USER, values, selection,
+					selectionArgs);
+			break;
+		case USER_ID:
+			String id = uri.getLastPathSegment();
+			if (TextUtils.isEmpty(selection)) {
+				rowsUpdated = sqlDB.update(UserTable.TABLE_USER, values,
+						UserTable.COLUMN_ID + "=" + id, null);
+			} else {
+				rowsUpdated = sqlDB.update(UserTable.TABLE_USER, values,
+						UserTable.COLUMN_ID + "=" + id + " and " + selection,
+						selectionArgs);
+			}
+			break;
+		default:
+			throw new IllegalArgumentException("Unknown URI: " + uri);
+		}
+		getContext().getContentResolver().notifyChange(uri, null);
+		return rowsUpdated;
+	}
+
+	private void checkColumns(String[] projection) {
+		String[] available = { UserTable.COLUMN_AGE, UserTable.COLUMN_NAME,
+				UserTable.COLUMN_ID };
+
+		if (projection != null) {
+			HashSet<String> requestedColumns = new HashSet<String>(
+					Arrays.asList(projection));
+			HashSet<String> availableColumns = new HashSet<String>(
+					Arrays.asList(available));
+			// check if all columns which are requested are available
+			if (!availableColumns.containsAll(requestedColumns)) {
+				throw new IllegalArgumentException(
+						"Unknown columns in projection");
+			}
+		}
+	}
+}
---
+++ src/ntnu/master/nofall/database/UserTable.java
@@ -0,0 +1,35 @@
+package ntnu.master.nofall.database;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+public class UserTable {
+	// Database table
+	public static final String TABLE_USER = "user";
+	public static final String COLUMN_ID = "_id";
+	public static final String COLUMN_NAME = "name";
+	public static final String COLUMN_AGE = "age";
+
+	// Database creation SQL statement
+	  private static final String DATABASE_CREATE = "create table " 
+		      + TABLE_USER
+		      + "(" 
+		      + COLUMN_ID + " integer primary key autoincrement, " 
+		      + COLUMN_NAME + " text not null," 
+		      + COLUMN_AGE
+		      + " text not null" 
+		      + ");";
+
+	public static void onCreate(SQLiteDatabase database) {
+		database.execSQL(DATABASE_CREATE);
+	}
+
+	public static void onUpgrade(SQLiteDatabase database, int oldVersion,
+			int newVersion) {
+		Log.w(UserTable.class.getName(), "Upgrading database from version "
+				+ oldVersion + " to " + newVersion
+				+ ", which will destroy all old data");
+		database.execSQL("DROP TABLE IF EXISTS " + TABLE_USER);
+		onCreate(database);
+	}
+}
